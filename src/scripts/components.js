de1ffa0de7e297fe37262b2ced46a7272e65ed49
Evan Carr <ejrcarr@gmail.com>
2023-03-15 15:58:08 -0400

    Fixed forever loading on bad api calls

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -308,8 +308,19 @@ const searchCityWeather = (isF) => {
 
 function handleSearch(isF) {
 	const searchBar = document.getElementById('search-city');
-	MainPage(searchBar.value, isF);
+	MainPage(searchBar.value.replaceAll(' ', '+'), isF);
 	searchBar.value = '';
+	setLoading();
+	setTimeout(fixLoading, 2000);
+}
+
+function fixLoading() {
+	const currentWeatherTile = document.querySelector('.current-weather-tile');
+	if (currentWeatherTile.childNodes.length > 1) {
+		return;
+	} else {
+		MainPage('Phoenix', true);
+	}
 }
 
 function updateBodyImage(iconId) {
@@ -335,6 +346,26 @@ function updateBackgroundTransparency(iconId) {
 	} else {
 		content.classList.remove('compensate');
 	}
+}
+
+function setLoading() {
+	const loader = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+      <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+        <animateTransform 
+           attributeName="transform" 
+           attributeType="XML" 
+           type="rotate"
+           dur="1s" 
+           from="0 50 50"
+           to="360 50 50" 
+           repeatCount="indefinite" />
+    </path>
+  </svg>`;
+	const weatherTiles = document.querySelectorAll('.daily-weather-tile');
+	weatherTiles.forEach((tile) => (tile.innerHTML = loader));
+	const currentWeatherTile = document.querySelector('.current-weather-tile');
+	currentWeatherTile.innerHTML = loader;
 }
 
 // IMAGE CREDITS
